07a82024bea79d27603bc60cb25c3c88560e739c
Siseum <62826105+Siseum@users.noreply.github.com>
2022-04-16 14:44:06 +0900

    Create x만큼 간격이 있는 n개의 숫자.java

---
+++ x만큼 간격이 있는 n개의 숫자.java
@@ -0,0 +1,17 @@
+class Solution 
+{
+    public long[] solution(int x, int n) 
+    {
+        long[] answer = new long[n];
+        long number = 0;
+        
+        for(int i=0;i<n;i++)
+        {
+            number +=x;
+            answer[i] = number;
+        }
+        
+        return answer;
+    }
+}
+   
