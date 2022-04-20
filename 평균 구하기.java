ccab29e8263febe3ae558479dd2d4f7e052db662
Siseum <62826105+Siseum@users.noreply.github.com>
2022-04-20 12:57:50 +0900

    Create 평균 구하기.java

---
+++ 평균 구하기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution 
+{
+    public double solution(int[] arr) 
+    {
+        double answer = 0;
+        
+        answer = Arrays.stream(arr).average();
+        
+        return answer;
+    }
+}
