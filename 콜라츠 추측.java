e7dd7eea31abe71c30aa60527f56fca60b0a00ef
Siseum <62826105+Siseum@users.noreply.github.com>
2022-04-21 15:55:01 +0900

    Create 콜라츠 추측.java

---
+++ 콜라츠 추측.java
@@ -0,0 +1,23 @@
+class Solution 
+{
+    public int solution(int num) 
+    {
+        int answer = 0;
+        long tempt = num;
+        
+        while(tempt != 1 && answer <= 500)
+        {
+            if(tempt % 2 ==0)
+                tempt = tempt / 2;
+            else
+                tempt = tempt * 3 +1;
+            
+            answer++;
+        }
+        
+        if(answer >500)
+            answer = -1;
+        
+        return answer;
+    }
+}
