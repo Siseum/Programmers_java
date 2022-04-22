738e710673b6a5e815a5164adc8d98351fbb187b
Siseum <62826105+Siseum@users.noreply.github.com>
2022-04-22 21:59:21 +0900

    Update 문자열 내림차순으로 배치하기.java

--- 문자열 내림차순으로 배치하기.java
+++ 문자열 내림차순으로 배치하기.java
@@ -5,11 +5,11 @@ class Solution
     public String solution(String s) 
     {
         String answer = "";
-		    String[] sArr = s.split("");
+	String[] sArr = s.split("");
 		
-		    Arrays.sort(sArr,Collections.reverseOrder());
+	Arrays.sort(sArr,Collections.reverseOrder());
        
-		    answer = String.join("", sArr);
+	answer = String.join("", sArr);
         
         return answer;
     }
