7d4414ba0679b633424560a69aafa676bca1e5b0
Siseum <62826105+Siseum@users.noreply.github.com>
2022-04-22 21:58:46 +0900

    Create 문자열 내림차순으로 배치하기.java

---
+++ 문자열 내림차순으로 배치하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution 
+{
+    public String solution(String s) 
+    {
+        String answer = "";
+		    String[] sArr = s.split("");
+		
+		    Arrays.sort(sArr,Collections.reverseOrder());
+       
+		    answer = String.join("", sArr);
+        
+        return answer;
+    }
+}
