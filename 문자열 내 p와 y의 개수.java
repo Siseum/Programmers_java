8487de79a55af6f8cff227b00d956cedc5d17cea
Siseum <62826105+Siseum@users.noreply.github.com>
2022-04-23 09:44:55 +0900

    Create 문자열 내 p와 y의 개수.java

---
+++ 문자열 내 p와 y의 개수.java
@@ -0,0 +1,24 @@
+class Solution 
+{
+    boolean solution(String s) 
+    {
+        boolean answer = true;
+        s = s.toUpperCase();
+
+        int pCount = 0;
+        int yCount = 0;
+        
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i) == 'P')
+                pCount++;
+            else if(s.charAt(i) == 'Y')
+                yCount++;
+        }
+            
+        if(pCount != yCount)
+            answer = false;
+
+        return answer;
+    }
+}
