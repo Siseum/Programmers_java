43903ea7e2305fa9050dd997c9a31d080ca2a3b7
Siseum <62826105+Siseum@users.noreply.github.com>
2022-05-13 10:20:18 +0900

    Create JadenCase 문자열 만들기.java

---
+++ JadenCase 문자열 만들기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+
+        StringBuffer sb = new StringBuffer(s);
+        boolean isFirst = true;
+        for(int i=0;i<sb.length();i++){
+            if(sb.charAt(i) == ' '){
+                isFirst=true;
+                continue;
+            }
+
+            if(isFirst == true) {
+                sb.setCharAt(i, Character.toUpperCase(sb.charAt(i)));
+                isFirst = false;
+            }
+            else{
+                sb.setCharAt(i,Character.toLowerCase(sb.charAt(i)));
+            }
+        }
+
+        answer = sb.toString();
+        
+        return answer;
+    }
+}
