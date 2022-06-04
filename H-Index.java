c90b8902d29d67c471fd8d3f347b8edd4f71e2f1
Siseum <62826105+Siseum@users.noreply.github.com>
2022-06-04 09:44:32 +0900

    Create H-Index.java

---
+++ H-Index.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+
+        Arrays.sort(citations);
+
+        for(int i=1;i<= citations.length;i++)
+        {
+            int count = 0;
+            for(int j=0;j<citations.length;j++)
+            {
+                if(citations[j]>=i)
+                    count++;
+                if(count == i)
+                    break;
+            }
+
+            answer = Math.max(answer, count);
+        }
+        
+        return answer;
+    }
+}
